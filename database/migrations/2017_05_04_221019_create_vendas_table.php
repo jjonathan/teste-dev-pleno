0b70380c8edd827850cbeac05d2430ea0b2062df
Jonathan Maria Machado <Jonathan Maria Machado>
2017-05-04 19:20:51 -0300

    Adicionado as migrations

--- database/migrations/2017_05_04_221019_create_vendas_table.php
+++ database/migrations/2017_05_04_221019_create_vendas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateVendasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('vendas', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('vendedor_id');
+            $table->float('valor_venda');
+            $table->dateTime('dt_venda');
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::drop('vendas');
     }
 }
