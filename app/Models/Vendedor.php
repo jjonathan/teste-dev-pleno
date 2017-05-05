c200cf2b343c4636bccc2b8f2cf167de45fcd1a4
Jonathan Machado <jonathan.mmachado@outlook.com>
2017-05-04 22:14:38 -0300

    Criado a classe de modelagem das tabelas

---
+++ app/Models/Vendedor.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Vendedor extends Model
+{
+    protected $table = 'vendedores';
+
+    protected $guarded = [];
+}
